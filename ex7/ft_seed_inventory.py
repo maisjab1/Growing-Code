2ca3f5f8cd3bc1c799a773af5bf7df3f67f7f74c
maisjab1 <142681219+maisjab1@users.noreply.github.com>
2026-02-14 14:16:19 +0300

    Add files via upload

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,13 +1,15 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     x1 = "available"
     x2 = "total"
-    x3 = "meters"
+    x3 = "square meters"
     if (unit == "packets"):
         x = x1
+        print(seed_type, " seeds: ", quantity, unit, x)
     elif (unit == "grams"):
         x = x2
+        print(seed_type, " seeds: ", quantity, unit, x)
     elif (unit == "area"):
         x = x3
+        print(seed_type, " seeds: covers ", quantity, x)
     else:
-        x = "Unknown unit type"
-    print(seed_type, " seeds: ", quantity, unit, x)
+        print("Unknown unit type")
