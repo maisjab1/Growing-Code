39e013b36af682e12f9a5e67869cf310072c495a
maisjab1 <142681219+maisjab1@users.noreply.github.com>
2026-02-21 03:41:17 +0300

    Add files via upload

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,10 +1,11 @@
 def ft_count_harvest_recursive():
     Days_until_harvest = int(input("Days until harvest:"))
-    for i in range(1, Days_until_harvest + 1, 1):
-        ft_counter(i, Days_until_harvest)
+    ft_counter(1, Days_until_harvest)
     print("Harvest time!")
 
 
 def ft_counter(i, Days_until_harvest):
-    if (i <= Days_until_harvest):
-        print("Day ", i)
+    if i > Days_until_harvest:
+        return
+    print("Day ", i)
+    ft_counter(i + 1, Days_until_harvest)
